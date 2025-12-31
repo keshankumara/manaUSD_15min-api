d08adf3b235ff811c943e0175f1350a955bf5fdb
keshankumara <eg245153@engug.ruh.ac.lk>
2025-12-31 16:33:02 +0530

    Add API_URL configuration to the settings

--- config.py
+++ config.py
@@ -3,3 +3,4 @@ SYMBOL = "MANAUSDT"
 INTERVAL = "15m"
 LIMIT = 20
 BINANCE_KLINES_URL = "https://api.binance.com/api/v3/klines"
+API_URL = "https://unerodable-peakishly-meghan.ngrok-free.dev/candles"
